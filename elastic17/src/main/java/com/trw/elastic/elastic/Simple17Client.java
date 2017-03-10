18a5b005f6f97f06c15ee02d4aeaa8f4c5430574
Thomas Weissinger <tweissin@cisco.com>
2017-03-10 12:16:15 -0500

    get Elastic 1.7 and 5.1 test clients working

--- elastic17/src/main/java/com/trw/elastic/elastic/Simple17Client.java
+++ elastic17/src/main/java/com/trw/elastic/elastic/Simple17Client.java
@@ -1,5 +1,15 @@
 package com.trw.elastic.elastic;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
@@ -8,13 +18,36 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import java.io.IOException;
 import java.util.Date;
 
-import static org.elasticsearch.common.xcontent.XContentFactory.*;
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 /**
  * Created by tweissin on 3/9/17.
  */
 public class Simple17Client {
     public static void main(String[] args) throws IOException {
+
+        createIndexWithTransportClient();
+        createIndexWithHttpClient();
+    }
+
+    private static void createIndexWithHttpClient() throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:9200/twitter");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonParser jp = new JsonParser();
+            String json = EntityUtils.toString(entity1);
+            JsonElement je = jp.parse(json);
+            System.out.println(gson.toJson(je));
+        } finally {
+            response1.close();
+        }
+    }
+
+    private static void createIndexWithTransportClient() throws IOException {
         try (
                 Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300))
         ) {
@@ -28,7 +61,6 @@ public class Simple17Client {
                     )
                     .execute()
                     .actionGet();
-
         }
     }
 }
--- elastic51/src/main/java/com/trw/elastic/Simple51Client.java
+++ elastic51/src/main/java/com/trw/elastic/Simple51Client.java
@@ -1,7 +1,69 @@
 package com.trw.elastic;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Date;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+
 /**
  * Created by tweissin on 3/9/17.
  */
 public class Simple51Client {
+    public static void main(String[] args) throws IOException {
+
+        createIndexWithTransportClient();
+        createIndexWithHttpClient();
+    }
+
+    private static void createIndexWithHttpClient() throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:9200/twitter");
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonParser jp = new JsonParser();
+            String json = EntityUtils.toString(entity1);
+            JsonElement je = jp.parse(json);
+            System.out.println(gson.toJson(je));
+        } finally {
+            response1.close();
+        }
+    }
+
+    private static void createIndexWithTransportClient() throws IOException {
+        try (
+                TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+        ) {
+            IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
+                    .setSource(jsonBuilder()
+                            .startObject()
+                            .field("user", "kimchy")
+                            .field("postDate", new Date())
+                            .field("message", "trying out Elasticsearch")
+                            .endObject()
+                    )
+                    .execute()
+                    .actionGet();
+        }
+    }
 }
