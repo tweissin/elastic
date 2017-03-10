e04c84707934445ce27f095c4f1ca915e6cc4690
Thomas Weissinger <tweissin@cisco.com>
2017-03-10 09:29:13 -0500

    use 1.8

--- elastic17/src/main/java/com/trw/elastic/elastic/Simple17Client.java
+++ elastic17/src/main/java/com/trw/elastic/elastic/Simple17Client.java
@@ -1,9 +1,34 @@
 package com.trw.elastic.elastic;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
+import java.io.IOException;
+import java.util.Date;
+
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
+
 /**
  * Created by tweissin on 3/9/17.
  */
 public class Simple17Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        try (
+                Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300))
+        ) {
+            IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
+                    .setSource(jsonBuilder()
+                            .startObject()
+                            .field("user", "kimchy")
+                            .field("postDate", new Date())
+                            .field("message", "trying out Elasticsearch")
+                            .endObject()
+                    )
+                    .execute()
+                    .actionGet();
 
+        }
     }
 }
