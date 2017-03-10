bd2ed2a35b0daaf018060197c8cc12e105f31b48
Thomas Weissinger <tweissin@cisco.com>
2017-03-10 08:24:04 -0500

    initial

---
+++ elastic17/src/main/java/com/trw/elastic/elastic/Simple17Client.java
@@ -0,0 +1,9 @@
+package com.trw.elastic.elastic;
+/**
+ * Created by tweissin on 3/9/17.
+ */
+public class Simple17Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ elastic51/src/main/java/com/trw/elastic/Simple51Client.java
@@ -0,0 +1,7 @@
+package com.trw.elastic;
+
+/**
+ * Created by tweissin on 3/9/17.
+ */
+public class Simple51Client {
+}
